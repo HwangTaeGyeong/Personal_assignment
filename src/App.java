0a9a5ebe6a63bc789dda98f40704c1e3da7ac374
HwangTaeGyeong <https://github.com/HwangTaeGyeong>
2024-04-29 16:01:57 +0900

    first requirement

--- src/App.java
+++ src/App.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("첫 번째 숫자를 입력하세요: ");
         int firstNumber = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
         int secondNumber = sc.nextInt();
+
+        System.out.print("사칙연산 기호를 입력하세요:");
+        char symbol = sc.next().charAt(0);
     }
 }
